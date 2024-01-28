b96dd0dcd60c086bede184a40a522e441cb2fc1c
Niklas <account@niklas.ai>
2024-01-28 16:31:56 +0100

    added article collection

---
+++ src/collections/Articles.ts
@@ -0,0 +1,33 @@
+import { CollectionConfig } from "payload/types";
+
+export const Articles: CollectionConfig = {
+  slug: "articles",
+  upload: {
+    staticURL: "/articles",
+    staticDir: "articles",
+    imageSizes: [
+      {
+        name: "thumbnail",
+        width: 400,
+        height: undefined,
+        position: "centre",
+      },
+    ],
+    adminThumbnail: "thumbnail",
+    mimeTypes: ["image/*"],
+  },
+  fields: [
+    {
+      name: "alt",
+      type: "text",
+    },
+    {
+      name: "title",
+      type: "text",
+    },
+    {
+      name: "content",
+      type: "richText",
+    },
+  ],
+};
